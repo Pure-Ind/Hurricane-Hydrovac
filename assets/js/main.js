5fb66e165b637b984f6ecb0c5fe6b781507aa80f
Mark DiCarlo <markdicarlo@gmail.com>
2020-05-19 14:46:48 -0400

    Adam changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,6 +9,7 @@ $("#main-header").headroom();
 
 $(document).ready(function(){
     $('.slick-slider').slick({
+        autoplay: true,
         prevArrow: '<div class="slick-prev"><img src="/assets/images/icons/chevron-left.svg" /></div>',
         nextArrow: '<div class="slick-next"><img src="/assets/images/icons/chevron-right.svg" /></div>'
     });
